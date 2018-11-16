4c8413708d3c5113235163557dc966e4364f8756
GeeNkonzo <gcobisankonzo@gmail.com>
2018-11-16 16:20:47 +0200

    dummy index.php added to code prototype array

---
+++ img/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet"> 
+    <title>Grey's anatomy quiz</title>
+</head>
+<body>
+    <header class = "banner">
+        <div class="cover"></div>
+       <p>Grey's Anatomy<br> Quiz<br>
+       <img class = "arrow" src = "img/down-arrow.png"></p>
+    </header>
+    
+    <div class="container">
+        <div class="main">
+            I just need to see my content 
+        </div>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,16 +3,213 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="css/main.css">
-    <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet"> 
-    <title>Grey's anatomy quiz</title>
+    <title>Page Title</title>
 </head>
 <body>
-    <header class = "banner">
-        <div class="poes"></div>
-       <p>Grey's Anatomy<br> Quiz</p>
-       <!-- <img class = "arrow" src = "img/down-arrow.png"> -->
-    </header>
-    
+   <?php
+        $questionnaire = array('q1'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q2'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q3'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q4'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q5'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q6'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q7'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q8'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q9'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q10'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q11'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q12'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q13'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q14'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q15'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q16'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q17'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q18'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q19'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                                'q20'=>
+                                    array('question' => "Which of the following has not been the name of the hospital?",
+                                            'answers' => 
+                                                array(
+                                                    1 => 'Grey Sherpherd Memorial Hospital',
+                                                    2 => 'Seattle Grace Mercy West Hospital',
+                                                    3 => 'Seattle Grace Hospital',
+                                                    4 => 'Grey Sloan Memorial Hospital'
+                                                    )
+                                                ),
+                            );
+
+    var_dump($questionnaire);
+   ?>
 </body>
 </html>
